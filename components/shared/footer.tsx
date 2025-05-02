e60ed4701d0fbd0f5fde61ef0f2b0d337460f0b1
muss001998 <mustapha001hamiai@gmail.com>
2025-05-02 22:54:38 +0300

    07-create-todays-deals-slider

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ChevronUp } from 'lucide-react'
-import { Button } from '../../ui/button'
+import { Button } from '../ui/button'
 import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
 
