3c07e2d7eb723b9a6975428936a60abf1b4f1c58
muss001998 <mustapha001hamiai@gmail.com>
2025-05-26 00:51:49 +0300

    21-rate-review-products

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,23 @@
-import { CartSchema, OrderInputSchema, OrderItemSchema, ProductInputSchema, ShippingAddressSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema } from '@/lib/validator'
+import { CartSchema, OrderInputSchema, OrderItemSchema, ProductInputSchema, ReviewInputSchema, ShippingAddressSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema } from '@/lib/validator'
 import { z } from 'zod'
 
+export type IReviewInput = z.infer<typeof ReviewInputSchema>
+export type IReviewDetails = IReviewInput & {
+  _id: string
+  createdAt: string
+  user: {
+    name: string
+  }
+}
 export type IProductInput = z.infer<typeof ProductInputSchema>
 export type Data = {
   users: IUserInput[]
   products: IProductInput[]
+  reviews: {
+    title: string
+    rating: number
+    comment: string
+  }[]
   headerMenus: {
     name: string
     href: string
